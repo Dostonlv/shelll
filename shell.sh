01d4212d17d57df19b4fff0397e2095f00a83cc4
Doston <IamitachiUchixa@gmail.com>
2022-05-31 22:53:46 +0500

    Goodbye World

--- shell.sh
+++ shell.sh
@@ -7,16 +7,16 @@ while true
 do
 	i=$((i+1))
 
-	if [ $i -gt 5000 ]
+	if [ $i -gt 400 ]
 		then
 			git push origin
 			i=0
 	fi
 
-	echo "a" >> a
+	echo "Goodbye World" >> a
 
 	git add . > /dev/null
-	git commit -m "a" > /dev/null
+	git commit -m "Goodbye World" > /dev/null
 
 	rm a
 
